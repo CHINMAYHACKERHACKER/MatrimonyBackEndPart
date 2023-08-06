af91a6ff2779ee89c73d09b7c830b15867389442
CHINMAY <95235771+CHINMAYHACKERHACKER@users.noreply.github.com>
2023-08-06 12:03:29 +0530

    Update server.ts

--- source/server.ts
+++ source/server.ts
@@ -62,14 +62,6 @@ const corsOptions = {
 };
 
 router.use(cors());
-
-router.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://matrimony-6ymt.onrender.com');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
-
 router.use(cors(corsOptions));
 
 /** Logging the request */
