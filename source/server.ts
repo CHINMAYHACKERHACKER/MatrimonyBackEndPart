6fff33103fe5603127ec626a5485ae177fcce4bc
CHINMAY <95235771+CHINMAYHACKERHACKER@users.noreply.github.com>
2023-08-06 10:59:08 +0530

    Update server.ts

--- source/server.ts
+++ source/server.ts
@@ -63,6 +63,13 @@ const corsOptions = {
 
 router.use(cors());
 
+router.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://matrimony-theta.vercel.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 router.use(cors(corsOptions));
 
 /** Logging the request */
